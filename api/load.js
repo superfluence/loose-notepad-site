f1f5961b627fad796d5ff391d210c3dcc1c4e613
superfluence <mark@loose.fm>
2025-12-03 13:17:58 +0000

    Add load API

---
+++ api/load.js
@@ -0,0 +1,11 @@
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export default async function handler(req, res) {
+  const content = await redis.get('content');
+  res.status(200).json({ content: content || '' });
+}
